4283064adaa370de5661d4b5eed3c6a3f0f8dc60
takishun0326 <takishun.st3260@gmail.com>
2022-06-09 16:41:44 +0900

    cropPos bottomUP

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ import os
 from PIL import Image
 
 # 画像の中心を切り出し
-def CropCenter(img, target_width, target_height):
+def CropCenter(img, target_width, target_height, amt_shift):
     return img.crop(((img.width - target_width) // 2,
-     (img.height - target_height) // 2,
+     (img.height - target_height) // 2 + amt_shift,
      (img.width + target_width) // 2,
-     (img.height + target_height) // 2))
+     (img.height + target_height) // 2 + amt_shift))
 
 
 # 最大サイズの正方形切り出し
-def CropMaxSquare(img):
-    return CropCenter(img, min(img.size), min(img.size))
+def CropMaxSquare(img, amt_shift):
+    return CropCenter(img, min(img.size), min(img.size), amt_shift)
 
 # アスペクト比維持リサイズ
 def KeepAspectImg(img, target_width, target_height):
@@ -30,7 +30,7 @@ def KeepAspectImg(img, target_width, target_height):
 
 
 # 正方形切り出しの画像リサイズmaster
-def ImgResizeSquare(i_path, o_path, target_width, target_height):
+def ImgResizeSquare(i_path, o_path, target_width, target_height, amt_shift):
     
     files = os.listdir(i_path)
     
@@ -38,7 +38,7 @@ def ImgResizeSquare(i_path, o_path, target_width, target_height):
         img = Image.open(os.path.join(i_path, file))
         
         # 最大サイズの正方形切り出し → 目的のサイズへリサイズ
-        cropped_img = CropMaxSquare(img)
+        cropped_img = CropMaxSquare(img, amt_shift)
         resized_img = KeepAspectImg(cropped_img, target_width, target_height)
 
         # 画像保存
@@ -61,10 +61,13 @@ if __name__ == "__main__":
     RESIZE_WIDTH_DEFAULT = 256
     RESIZE_HEIGHT_DEFAULT = 256
 
+    # 正方形切り出しの位置を上げる量 負: 上へシフト, 正: 下へシフト
+    AMOUNT_SHIFT = -240
+
     # 入力, 出力のパス取得
     i_path, o_path = GetInputOutputPath()
 
-    ImgResizeSquare(i_path, o_path, RESIZE_WIDTH_DEFAULT, RESIZE_HEIGHT_DEFAULT)
+    ImgResizeSquare(i_path, o_path, RESIZE_WIDTH_DEFAULT, RESIZE_HEIGHT_DEFAULT, AMOUNT_SHIFT)
 
 
 
